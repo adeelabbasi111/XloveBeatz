b930ed92af3db9de684372d1cb7ce546fe79ec09
Adeel Abbasi <adeelabbasipersonal@gmail.com>
2026-08-26 20:27:16 +0500

    Clean up temporary python script replace.py

--- replace.py
+++
@@ -1,3 +0,0 @@
-content = open('templates/index.html', 'r', encoding='utf-8').read()
-content = content.replace('class="hero-image" ', 'class="hero-image" fetchpriority="high" ')
-open('templates/index.html', 'w', encoding='utf-8').write(content)
